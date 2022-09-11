8253c279e483d6b08059d7ab4498ccf313e380a3
KennethNyariro <nyarirokenneth18@gmail.com>
2022-09-11 05:44:29 -0700

    Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main-print the string in the printf function
+ *
+ * Description: using the main function
+ * this program prints "Size of a: char, int, long int, long long int, float
+ * Return: 0
+ */
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("Size of a char: %ld byte(s)\n", (unsigned long)size0f(c));
+	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %d bytes(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
+
