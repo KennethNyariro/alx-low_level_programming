300bb4244ad3410e5954f59376c8de8e3df8a6ea
KennethNyariro <nyarirokenneth18@gmail.com>
2022-09-11 12:52:01 -0700

    UNIX is a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main-print te string in the write function
+ *
+ * Description: this program prints "and that piece of art is useful" -\n
+ * Dora Korpar, 2015-10-19
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n
+", 59);
+	return (1);
+
+}
