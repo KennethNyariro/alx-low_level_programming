0a5d7d22bec47e3e2ad46d5b30fdee6ce969e485
KennethNyariro <nyarirokenneth18@gmail.com>
2022-09-11 07:55:09 -0700

    Intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,17 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
 
-	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long int: %ld byte(s)\n", (long unsigned int)sizeof(li));
-	printf("Size of a long long int: %lld byte(s)\n", (long long unsigned int)sizeof(lli));
-	printf("Size of a float: %ld bytes(s)\n", (unsigned long)sizeof(f));
-	return (0);
+       char c;
+    int i;
+    long li;
+    long long lli;
+    float f;
+
+    printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
+    printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(li));
+    printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(li)));
+    printf("Size of a long long int: %lld byte(s)\n", (unsigned long long)sizeof(lli));
+    printf("Size of a float: %ld bytes(s)\n", (unsigned long)sizeof(f));
+    return (0);
 }
