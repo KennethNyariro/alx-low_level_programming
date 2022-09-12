d5ddd7630942c0b0627b8f63a5dba0a638319b12
KennethNyariro <nyarirokenneth18@gmail.com>
2022-09-11 20:56:10 -0700

    Size is not granduer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
 
 /**
- * main-print the string in the printf function
+ * main-print the string in the put function
  *
  * Description: using the main function
- * this program prints "Size of various types on the computer
+ * this program "prints size of various types on the computer
  * Return: 0
  */
-int main(void)
+int main(viod)
 {
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
 
-       char c;
-    int i;
-    long li;
-    long long lli;
-    float f;
+	printf("Size of char: %ld byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of int: %ld byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of long long int: %lld byte(s)\n",
+	(unsigned long long)sizeof(lli));
+	printf("Size of float: %ld byte(s)\n", (unsigned long)sizeof(f));
 
-    printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
-    printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(li));
-    printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(li)));
-    printf("Size of a long long int: %lld byte(s)\n", (unsigned long long)sizeof(lli));
-    printf("Size of a float: %ld bytes(s)\n", (unsigned long)sizeof(f));
-    return (0);
+	return (0);
 }
