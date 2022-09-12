4d3d1833e2bb49369bfc0b6b0dc1dbce280fb33f
KennethNyariro <nyarirokenneth18@gmail.com>
2022-09-11 19:59:26 -0700

    Unix is a simple language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main-print te string in the write function
  *
- * Description: this program prints "and that piece of art is useful" -\n
- * Dora Korpar, 2015-10-19
+ * this program prints "and that piece of art is useful\" - Dora Korpar\n
+ * , 2015-10-19\n", 59);
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n
-", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
 }
